4e69e580351ed284be01d3fd348c8919c76d175e
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-05-09 18:45:04 +0800

    Add job preference

--- assets/js/formSection.js
+++ assets/js/formSection.js
@@ -3,34 +3,48 @@ $(document).ready(function(){
     * show side bar ehrn arrow forward is clicked
     * change the arrow forward with arrow back
     */
-    
-
     let openSideBar = () => {
         $(".form-section").animate({left: '0px'})
         $("#arrow-forward").css("display", "none");
         $("#arrow-back").css("display", "block");
     }
     let closeSideBar = () => {
-        $(".form-section").animate({left: '-200px'});
-        $("#arrow-forward").css("display", "block");
-        $("#arrow-back").css("display", "none");
+        /* Only execute this when the screen size is equal or less than 576 pixels width */
+        if ($(window).width() <= 576) {
+            $(".form-section").animate({left: '-200px'});
+            $("#arrow-forward").css("display", "block");
+            $("#arrow-back").css("display", "none");
+        }
     }
-    
+    /* Open the side bar when arrow forward icon is clicked */
     $(document).on('click', "#arrow-forward", function(){
        openSideBar();
-       $('#navbarNav').collapse('hide');
+       $('#navbarNav').collapse('hide'); /* Hide navbar*/
     });
+    /* Close side bar when arrow back icon is clicked */
     $(document).on('click', '#arrow-back', function(){
         closeSideBar();
     });
-
+    /* Set the font color of input date when its value is changed */
     $("input[type='date']").change(function() {
         $(this).css('color', 'black');
     });
-
+    /* Hide the side bar when navbar (menu) icon is clicked */
     $(".navbar-toggler").click(function(){
         closeSideBar();
-        // let img = $('<img id="arrow-forward" src="/images/arrow_forward.svg" alt="Arrow forward icon">');
-        // $("#arrow-icon").css("left", "0");
+    });
+    
+    /**/
+    $("")
+    $("#next-job-preference").click(function(){
+        $(".personal-information").css("display", "none");
+        $(".job-preference").css("display", "block");
+
+        $("#personal-information").css("font-weight", "100");
+        $("#job-preference").css("font-weight", "bold")
+    })
+    $("#prev-personal-information").click(function(){
+        $(".personal-information").css("display", "block");
+        $(".job-preference").css("display", "none");
     })
 });
