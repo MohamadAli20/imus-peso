5b6e3bb318268aa9c98eece1da193f1b01e2c8c9
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-05-15 01:43:46 +0800

    Fix form page

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,17 +4,14 @@
 $(".btn-prev").off("click").on("click", function(e){
     e.preventDefault();
     
-    // if (currentPage > 1) {
-        /* remove the bold of the current link */
-        $(`.page${currentPage}`).css("font-weight", "100"); 
-        $(`#page${currentPage}`).css("display", "none");
+    $(`.page${currentPage}`).css("font-weight", "100"); 
+    $(`#page${currentPage}`).css("display", "none");
 
-        /* decrement the value of the current page */
-        currentPage -= 1;
-        $(`#page${currentPage}`).css("display", "block");
-        /* bold the selected link or the current page label in the sidebar */
-        $(`.page${currentPage}`).css("font-weight", "bold");
-    // }
+    /* decrement the value of the current page */
+    currentPage -= 1;
+    $(`#page${currentPage}`).css("display", "block");
+    /* bold the selected link or the current page label in the sidebar */
+    $(`.page${currentPage}`).css("font-weight", "bold");
 });
 
 /*
@@ -23,22 +20,13 @@ $(".btn-prev").off("click").on("click", function(e){
 $(".btn-next").off("click").on("click", function(e){
     e.preventDefault();
     
-    // let form = $(this).closest('form')[0];
-    // if (checkInputField(form)) {
-    //     if (currentPage < totalPageCount) { // Ensure totalPageCount is defined
-            /* remove the bold of the current link */
-            $(`.page${currentPage}`).css("font-weight", "100"); 
-            $(`#page${currentPage}`).css("display", "none");
+    $(`.page${currentPage}`).css("font-weight", "100"); 
+    $(`#page${currentPage}`).css("display", "none");
 
-            /* increment the value of the current page */
-            currentPage += 1;
-            console.log(currentPage)
-            $(`#page${currentPage}`).css("display", "block");
-            /* bold the selected link or the current page label in the sidebar */
-            $(`.page${currentPage}`).css("font-weight", "bold");
-    //     }
-    // } else {
-    //     // Handle form validation failure (optional)
-    //     alert("Please fill out all required fields.");
-    // }
+    /* increment the value of the current page */
+    currentPage += 1;
+    console.log(currentPage)
+    $(`#page${currentPage}`).css("display", "block");
+    /* bold the selected link or the current page label in the sidebar */
+    $(`.page${currentPage}`).css("font-weight", "bold");
 });
