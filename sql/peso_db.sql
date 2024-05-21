470d6a65f4f879cde9918b2912c7823e0c8cf5c1
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-05-22 00:12:21 +0800

    Add sql file

---
+++ sql/peso_db.sql
@@ -0,0 +1,223 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: peso_db
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `educational_background`
+--
+
+DROP TABLE IF EXISTS `educational_background`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `educational_background` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `elementary_school` varchar(255) DEFAULT NULL,
+  `elementary_course` varchar(255) DEFAULT NULL,
+  `elementary_year_graduated` varchar(45) DEFAULT NULL,
+  `if_elementary_undergraduate` varchar(255) DEFAULT NULL,
+  `secondary_school` varchar(255) DEFAULT NULL,
+  `secondary_course` varchar(255) DEFAULT NULL,
+  `secondary_year_graduated` varchar(45) DEFAULT NULL,
+  `if_secondary_undergraduate` varchar(255) DEFAULT NULL,
+  `tertiary_school` varchar(255) DEFAULT NULL,
+  `tertiary_course` varchar(255) DEFAULT NULL,
+  `tertiary_year_graduated` varchar(45) DEFAULT NULL,
+  `if_tertiary_undergraduate` varchar(255) DEFAULT NULL,
+  `graduate_studies_school` varchar(255) DEFAULT NULL,
+  `graduate_studies_course` varchar(255) DEFAULT NULL,
+  `gradudate_studies_year_attended` varchar(45) DEFAULT NULL,
+  `if_graduate_studies_undergraduate` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `eligibility_professional_license`
+--
+
+DROP TABLE IF EXISTS `eligibility_professional_license`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `eligibility_professional_license` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `eligibility` varchar(255) DEFAULT NULL,
+  `rating` varchar(255) DEFAULT NULL,
+  `date_exam` varchar(255) DEFAULT NULL,
+  `professional_license` varchar(255) DEFAULT NULL,
+  `valid_until` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `job_preference`
+--
+
+DROP TABLE IF EXISTS `job_preference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job_preference` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `preferred_occupation` varchar(255) DEFAULT NULL,
+  `occupation` varchar(255) DEFAULT NULL,
+  `preferred_work_occupation` varchar(255) DEFAULT NULL,
+  `work_occupation` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `language_dialect_proficiency`
+--
+
+DROP TABLE IF EXISTS `language_dialect_proficiency`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `language_dialect_proficiency` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `language1` varchar(255) DEFAULT NULL,
+  `language2` varchar(255) DEFAULT NULL,
+  `language3` varchar(255) DEFAULT NULL,
+  `other_language` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `other_skills`
+--
+
+DROP TABLE IF EXISTS `other_skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `other_skills` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `skills` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `personal_information`
+--
+
+DROP TABLE IF EXISTS `personal_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal_information` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `surname` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `middlename` varchar(45) DEFAULT NULL,
+  `suffix` varchar(45) DEFAULT NULL,
+  `birthdate` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `contact` varchar(45) DEFAULT NULL,
+  `height` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `civil_status` varchar(45) DEFAULT NULL,
+  `disability` varchar(255) DEFAULT NULL,
+  `religion` varchar(45) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `employment_status` varchar(255) DEFAULT NULL,
+  `is_ofw` varchar(255) DEFAULT NULL,
+  `is_former_ofw` varchar(255) DEFAULT NULL,
+  `is_4ps_beneficiary` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `technical_vocational_training`
+--
+
+DROP TABLE IF EXISTS `technical_vocational_training`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `technical_vocational_training` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course` varchar(255) DEFAULT NULL,
+  `institution` varchar(255) DEFAULT NULL,
+  `date_from` varchar(255) DEFAULT NULL,
+  `date_to` varchar(255) DEFAULT NULL,
+  `certificate` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `is_admin` tinyint DEFAULT NULL,
+  `password` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `work_experience`
+--
+
+DROP TABLE IF EXISTS `work_experience`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `work_experience` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) DEFAULT NULL,
+  `company_address` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `inclusive_date` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-22  0:10:53
