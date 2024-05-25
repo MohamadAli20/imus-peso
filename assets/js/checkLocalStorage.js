7d06ca86133b19ec5e10d4250a5aa067dca773c9
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-05-25 13:02:01 +0800

    Add user type authentication

--- assets/js/checkLocalStorage.js
+++ assets/js/checkLocalStorage.js
@@ -1,24 +1,33 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
     let isAdmin = localStorage.getItem('isAdmin');
+    
+    // Function to check account status and redirect accordingly
     let checkAccount = () => {
-        if(isAdmin == 1){
+        console.log("checkAccount called, isAdmin:", isAdmin);
+        
+        if(isAdmin === '1' && window.location.pathname !== "/dashboard"){
+            console.log("Redirecting to /dashboard");
             window.location.href = "/dashboard";
-        }
-        if(isAdmin == 0){
-            window.location.href = "/";
         } 
-    }
+        else if(isAdmin === '0' && window.location.pathname !== "/"){
+            console.log("Redirecting to /");
+            window.location.href = "/";
+        }
+        else if(isAdmin === null && window.location.pathname !== "/"){
+            console.log("Redirecting to /");
+            window.location.href = "/";
+        }
+    };
+    
+    // Perform account check once when the document is ready
     checkAccount();
     
-    $(".apply-now").click(function(){
-        // checkAccount();
-        if(isAdmin == null){
+    // Event handler for the .apply-now button click
+    $(".apply-now").click(function() {
+        if (isAdmin === null) {
             $("#navLoginBtn").trigger('click');
-        }
-        else{
-            window.location.href = "/apply"
+        } else {
+            window.location.href = "/apply";
         }
     });
-
 });
