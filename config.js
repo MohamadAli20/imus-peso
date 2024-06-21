9581ebf193bce0d84bb599aa2e895ccbadad6afc
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-06-22 00:13:51 +0800

    Add the config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+
+// const config = {
+//     host: 'pesodatabase.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'testing123',
+//     database: 'peso_db'
+// };
+
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'peso_db'
+};
+
+
+module.exports = config; 
