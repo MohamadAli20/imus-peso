b45174e35e2d920cd883a2886ae246e7b6abe0f2
Funnyonkey <renzmark.abut@cvsu.edu.ph>
2024-07-06 15:55:14 +0800

    Update

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// const config = {
-//     host: 'peso-sys-database.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
-//     user: 'admin',
-//     password: 'testing123',
-//     database: 'peso_db'
-// };
-
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'peso-sys-database.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'testing123',
     database: 'peso_db'
 };
+
+// const config = {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'peso_db'
+// };
 
 module.exports = config; 
