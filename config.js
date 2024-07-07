54cf1e8cabf3eb05857b60280e5a1a13ed896a79
Funnyonkey <renzmark.abut@cvsu.edu.ph>
2024-07-07 13:58:27 +0800

    commit

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
-
-// const config = {
-//     host: 'peso-sys-database.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
-//     user: 'admin',
-//     password: 'testing123',
-//     database: 'peso_db'
-// };
-
 const config = {
     host: 'localhost',
     user: 'root',
